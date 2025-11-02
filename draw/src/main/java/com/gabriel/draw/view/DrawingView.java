896babc594956207fe7ebe0dc6ff9e6ecf6cfc04
Gerome <gerome.vincent.carreon@gmail.com>
2025-11-02 18:35:04 +0800

    Commit

---
+++ draw/src/main/java/com/gabriel/draw/view/DrawingView.java
@@ -0,0 +1,41 @@
+package com.gabriel.draw.view;
+
+import com.gabriel.draw.controller.DrawingController;
+import com.gabriel.draw.controller.DrawingWindowController;
+import com.gabriel.drawfx.model.Drawing;
+import com.gabriel.drawfx.model.Shape;
+import com.gabriel.drawfx.service.AppService;
+import lombok.Setter;
+
+import java.util.List;
+import javax.swing.*;
+import java.awt.*;
+
+public class DrawingView extends JPanel {
+
+    AppService appService;
+
+    public DrawingView(AppService appService){
+        this.appService  = appService;
+        JTextArea textArea = new JTextArea();
+        add(textArea);
+        textArea.setVisible(true);
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        Drawing drawing = appService.getDrawing();
+        List<Shape> shapes  = drawing.getShapes();
+        for(Shape shape : shapes){
+            shape.getRendererService().render(g, shape, false);
+        }
+    }
+
+
+}
